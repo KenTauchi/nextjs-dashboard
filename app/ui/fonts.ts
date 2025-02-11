eb19f2de33de92fd0410a602dd79c5b19b8f8ffd
Ken Tauchi <kentauchica@gmail.com>
2025-02-10 22:21:36 -0800

    feat(init): initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lustiana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
